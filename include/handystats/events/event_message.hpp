b82373e61d14380f4606eeb8bc77e55172b1ee7c
Danil Osherov <shindo@yandex-team.ru>
2014-02-04 18:15:10 +0400

    events: event_message format has been added.

---
+++ include/handystats/events/event_message.hpp
@@ -0,0 +1,29 @@
+#ifndef HANDYSTATS_EVENT_MESSAGE_H_
+#define HANDYSTATS_EVENT_MESSAGE_H_
+
+#include <string>
+#include <vector>
+
+#include "handystats/chrono.hpp"
+
+namespace handystats { namespace events {
+
+enum class event_destination_type {
+	COUNTER,
+	GAUGE,
+	TRANSACTION
+};
+
+struct event_message {
+	std::string destination_name;
+	event_destination_type destination_type;
+
+	chrono::steady_clock<std::chrono::nanoseconds>::time_point timestamp;
+
+	void* event_type;
+	std::vector<void*> event_data;
+};
+
+}} // namespace handystats::events
+
+#endif // HANDYSTATS_EVENT_MESSAGE_H_

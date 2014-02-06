b79bb55bfde3154011b18cf7c7998d4055dd6dd9
Danil Osherov <shindo@yandex-team.ru>
2014-02-06 19:38:51 +0400

    tests: common tests for handy library have been added.

---
+++ tests/handy.test.cpp
@@ -0,0 +1,170 @@
+#include <vector>
+#include <utility>
+#include <cstdlib>
+#include <thread>
+#include <chrono>
+#include <queue>
+
+#include <gtest/gtest.h>
+
+#include "handystats/handystats.hpp"
+
+TEST(HandyCounterTest, HandyBubbleSortMonitoring) {
+	HANDY_ENABLE();
+
+	const int SIZE = 50;
+	std::vector<int> data(SIZE);
+	for (auto& element : data) {
+		element = rand();
+	}
+
+	int swaps_count = 0;
+	for (int step = 0; step < SIZE - 1; ++step) {
+		bool sorted = true;
+		for (int i = 0; i < SIZE - 1; ++i) {
+			if (data[i] > data[i + 1]) {
+				std::swap(data[i], data[i + 1]);
+				sorted = false;
+				swaps_count++;
+
+				HANDY_COUNTER_INCREMENT("swaps.count", 1);
+			}
+		}
+
+		if (sorted) {
+			break;
+		}
+	}
+
+	while (!handystats::message_queue::event_message_queue->empty()) {
+		std::this_thread::sleep_for(std::chrono::microseconds(10));
+	}
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	int handy_count =
+		boost::get<handystats::internal::internal_counter*>(handystats::internal::monitors["swaps.count"])
+		->base_metric
+		->get().first;
+
+	ASSERT_EQ(handy_count, swaps_count);
+
+	HANDY_DISABLE();
+}
+
+TEST(HandyGaugeTest, HandyQueueSizeMonitoring) {
+	HANDY_ENABLE();
+
+	const int OPER_COUNT = 5000;
+	std::queue<int> data;
+
+	int max_queue_size = 0;
+	for (int oper = 0; oper < OPER_COUNT; ++oper) {
+		if (rand() & 1) {
+			data.push(rand());
+		}
+		else {
+			if (!data.empty()) {
+				data.pop();
+			}
+		}
+		HANDY_GAUGE_SET("queue.size", data.size());
+
+		if (data.size() > max_queue_size) {
+			max_queue_size = data.size();
+		}
+	}
+
+	while (!handystats::message_queue::event_message_queue->empty()) {
+		std::this_thread::sleep_for(std::chrono::microseconds(10));
+	}
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	int handy_max_size =
+		boost::get<handystats::internal::internal_gauge*>(handystats::internal::monitors["queue.size"])
+		->base_metric
+		->get_stats().max_value;
+
+	ASSERT_EQ(handy_max_size, max_queue_size);
+
+	HANDY_DISABLE();
+}
+
+TEST(HandyInternalTest, CheckEqualCounterNamesOnRestart) {
+	HANDY_ENABLE();
+
+	const int value_1 = 10;
+	HANDY_COUNTER_INCREMENT("test.counter", value_1);
+
+	while (!handystats::message_queue::event_message_queue->empty()) {
+		std::this_thread::sleep_for(std::chrono::microseconds(10));
+	}
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	int handy_value_1 =
+		boost::get<handystats::internal::internal_counter*>(handystats::internal::monitors["test.counter"])
+		->base_metric
+		->get().first;
+
+	ASSERT_EQ(handy_value_1, value_1);
+
+	HANDY_DISABLE();
+
+	HANDY_ENABLE();
+
+	const int value_2 = 100;
+	HANDY_COUNTER_INCREMENT("test.counter", value_2);
+
+	while (!handystats::message_queue::event_message_queue->empty()) {
+		std::this_thread::sleep_for(std::chrono::microseconds(10));
+	}
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	int handy_value_2 =
+		boost::get<handystats::internal::internal_counter*>(handystats::internal::monitors["test.counter"])
+		->base_metric
+		->get().first;
+
+	ASSERT_EQ(handy_value_2, value_2);
+
+	HANDY_DISABLE();
+}
+
+TEST(HandyInternalTest, CheckEqualMetricNamesOnRestart) {
+	HANDY_ENABLE();
+
+	const int value_1 = 10;
+	HANDY_COUNTER_INCREMENT("test.metric", value_1);
+
+	while (!handystats::message_queue::event_message_queue->empty()) {
+		std::this_thread::sleep_for(std::chrono::microseconds(10));
+	}
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	int handy_value_1 =
+		boost::get<handystats::internal::internal_counter*>(handystats::internal::monitors["test.metric"])
+		->base_metric
+		->get().first;
+
+	ASSERT_EQ(handy_value_1, value_1);
+
+	HANDY_DISABLE();
+
+	HANDY_ENABLE();
+
+	const int value_2 = 100;
+	HANDY_GAUGE_SET("test.metric", value_2);
+
+	while (!handystats::message_queue::event_message_queue->empty()) {
+		std::this_thread::sleep_for(std::chrono::microseconds(10));
+	}
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	int handy_value_2 =
+		boost::get<handystats::internal::internal_gauge*>(handystats::internal::monitors["test.metric"])
+		->base_metric
+		->get().first;
+
+	ASSERT_EQ(handy_value_2, value_2);
+
+	HANDY_DISABLE();
+}

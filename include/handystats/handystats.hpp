5dccdd1161667a5e7911a617b06f755b50b57c9d
Danil Osherov <shindo@yandex-team.ru>
2014-02-06 17:29:48 +0400

    Main handystats header file has been added which should include all other necessary headers.

---
+++ include/handystats/handystats.hpp
@@ -0,0 +1,12 @@
+#ifndef HANDYSTATS_HANDYSTATS_H_
+#define HANDYSTATS_HANDYSTATS_H_
+
+#include "handystats/core.hpp"
+#include "handystats/internal.hpp"
+#include "handystats/message_queue.hpp"
+
+#include "handystats/message_queue/counter_event_message.hpp"
+#include "handystats/message_queue/gauge_event_message.hpp"
+#include "handystats/message_queue/transaction_event_message.hpp"
+
+#endif // HANDYSTATS_HANDYSTATS_H_

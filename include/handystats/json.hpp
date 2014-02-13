f786c5700c7eabb66df1991190f881fc852b1f74
Danil Osherov <shindo@yandex-team.ru>
2014-02-13 22:35:21 +0400

    json: common json header has been added.

---
+++ include/handystats/json.hpp
@@ -0,0 +1,9 @@
+#ifndef HANDYSTATS_JSON_WRITERS_H_
+#define HANDYSTATS_JSON_WRITERS_H_
+
+#include <handystats/rapidjson/document.h>
+
+#include <handystats/json/allocators.hpp>
+#include <handystats/json/gauge_json_writer.hpp>
+
+#endif // HANDYSTATS_JSON_WRITERS_H_

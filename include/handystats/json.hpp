22308fce243ae5fa157aee504327c28bc595bb60
Danil Osherov <shindo@yandex-team.ru>
2014-02-14 15:00:59 +0400

    json: common json header fixup.

--- include/handystats/json.hpp
+++ include/handystats/json.hpp
@@ -4,6 +4,9 @@
 #include <handystats/rapidjson/document.h>
 
 #include <handystats/json/allocators.hpp>
+#include <handystats/json/timestamp.hpp>
+
 #include <handystats/json/gauge_json_writer.hpp>
+#include <handystats/json/counter_json_writer.hpp>
 
 #endif // HANDYSTATS_JSON_WRITERS_H_

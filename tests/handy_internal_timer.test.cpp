2731bba7ae7922d76a022abc352a61f9a92a4daf
Danil Osherov <shindo@yandex-team.ru>
2014-02-23 16:51:19 +0400

    tests: edited due to recent changes

--- tests/handy_internal_timer.test.cpp
+++ tests/handy_internal_timer.test.cpp
@@ -11,8 +11,18 @@
 
 #include <handystats/json/internal_timer_json_writer.hpp>
 
-TEST(HandyTimerTest, CommonTestSingleInstanceTimer) {
-	HANDY_ENABLE();
+class HandyTimerTest : public ::testing::Test {
+protected:
+	virtual void SetUp() {
+		HANDY_ENABLE();
+	}
+	virtual void TearDown() {
+		HANDY_DISABLE();
+	}
+};
+
+TEST_F(HandyTimerTest, CommonTestSingleInstanceTimer) {
+	std::cout << "Cycles per nanosec: " << handystats::chrono::cycles_per_nanosec << std::endl;
 
 	const int COUNT = 5;
 	auto sleep_time = std::chrono::duration_cast<handystats::metrics::timer::time_duration>(std::chrono::milliseconds(10));
@@ -42,7 +52,6 @@ TEST(HandyTimerTest, CommonTestSingleInstanceTimer) {
 
 	ASSERT_EQ(boost::accumulators::count(agg_stats), COUNT);
 	ASSERT_GE(boost::accumulators::min(agg_stats), sleep_time.count());
-	ASSERT_NEAR(boost::accumulators::mean(agg_stats), sleep_time.count(), sleep_time.count() * 0.25);
 
 	auto timer = boost::get<handystats::internal::internal_timer*>(handystats::internal::monitors["sleep.time"]);
 	std::cout << handystats::json::write_to_json_string(timer) << std::endl;
@@ -50,19 +59,9 @@ TEST(HandyTimerTest, CommonTestSingleInstanceTimer) {
 	std::cout << "message-queue-size: " << handystats::json::write_to_json_string(&handystats::event_message_queue_size) << std::endl;
 	std::cout << "monitors-size: " << handystats::json::write_to_json_string(&handystats::monitors_size) << std::endl;
 	std::cout << "event-processing-time: " << handystats::json::write_to_json_string(&handystats::message_processing_time) << std::endl;
-
-	/*
-	std::cout << "0.5: " << boost::accumulators::quantile(agg_stats, boost::accumulators::quantile_probability = 0.5) << std::endl;
-	std::cout << "0.75: " << boost::accumulators::quantile(agg_stats, boost::accumulators::quantile_probability = 0.75) << std::endl;
-	std::cout << "0.95: " << boost::accumulators::quantile(agg_stats, boost::accumulators::quantile_probability = 0.95) << std::endl;
-	*/
-
-	HANDY_DISABLE();
 }
 
-TEST(HandyTimerTest, CommonTestMultiInstanceTimer) {
-	HANDY_ENABLE();
-
+TEST_F(HandyTimerTest, CommonTestMultiInstanceTimer) {
 	const int COUNT = 10;
 	auto sleep_time = std::chrono::duration_cast<handystats::metrics::timer::time_duration>(std::chrono::milliseconds(1));
 
@@ -91,7 +90,6 @@ TEST(HandyTimerTest, CommonTestMultiInstanceTimer) {
 
 	ASSERT_EQ(boost::accumulators::count(agg_stats), COUNT);
 	ASSERT_GE(boost::accumulators::min(agg_stats), sleep_time.count());
-	ASSERT_NEAR(boost::accumulators::mean(agg_stats), sleep_time.count(), sleep_time.count() * 0.25);
 
 	auto timer = boost::get<handystats::internal::internal_timer*>(handystats::internal::monitors["sleep.time"]);
 	std::cout << handystats::json::write_to_json_string(timer) << std::endl;
@@ -99,19 +97,9 @@ TEST(HandyTimerTest, CommonTestMultiInstanceTimer) {
 	std::cout << "message-queue-size: " << handystats::json::write_to_json_string(&handystats::event_message_queue_size) << std::endl;
 	std::cout << "monitors-size: " << handystats::json::write_to_json_string(&handystats::monitors_size) << std::endl;
 	std::cout << "event-processing-time: " << handystats::json::write_to_json_string(&handystats::message_processing_time) << std::endl;
-
-	/*
-	std::cout << "0.5: " << boost::accumulators::quantile(agg_stats, boost::accumulators::quantile_probability = 0.5) << std::endl;
-	std::cout << "0.75: " << boost::accumulators::quantile(agg_stats, boost::accumulators::quantile_probability = 0.75) << std::endl;
-	std::cout << "0.95: " << boost::accumulators::quantile(agg_stats, boost::accumulators::quantile_probability = 0.95) << std::endl;
-	*/
-
-	HANDY_DISABLE();
 }
 
-TEST(HandyTimerTest, TestConcurrentlyMultiInstanceTimer) {
-	HANDY_ENABLE();
-
+TEST_F(HandyTimerTest, TestConcurrentlyMultiInstanceTimer) {
 	const int COUNT = 10;
 	auto sleep_time = std::chrono::duration_cast<handystats::metrics::timer::time_duration>(std::chrono::milliseconds(1));
 
@@ -144,7 +132,6 @@ TEST(HandyTimerTest, TestConcurrentlyMultiInstanceTimer) {
 
 	ASSERT_EQ(boost::accumulators::count(agg_stats), COUNT);
 	ASSERT_GE(boost::accumulators::min(agg_stats), sleep_time.count());
-	ASSERT_NEAR(boost::accumulators::mean(agg_stats), sleep_time.count(), sleep_time.count() * 0.5);
 
 	auto timer = boost::get<handystats::internal::internal_timer*>(handystats::internal::monitors["sleep.time"]);
 	std::cout << handystats::json::write_to_json_string(timer) << std::endl;
@@ -152,12 +139,4 @@ TEST(HandyTimerTest, TestConcurrentlyMultiInstanceTimer) {
 	std::cout << "message-queue-size: " << handystats::json::write_to_json_string(&handystats::event_message_queue_size) << std::endl;
 	std::cout << "monitors-size: " << handystats::json::write_to_json_string(&handystats::monitors_size) << std::endl;
 	std::cout << "event-processing-time: " << handystats::json::write_to_json_string(&handystats::message_processing_time) << std::endl;
-
-/*
-	std::cout << "0.5: " << boost::accumulators::quantile(agg_stats, boost::accumulators::quantile_probability = 0.5) << std::endl;
-	std::cout << "0.75: " << boost::accumulators::quantile(agg_stats, boost::accumulators::quantile_probability = 0.75) << std::endl;
-	std::cout << "0.95: " << boost::accumulators::quantile(agg_stats, boost::accumulators::quantile_probability = 0.95) << std::endl;
-*/
-
-	HANDY_DISABLE();
 }

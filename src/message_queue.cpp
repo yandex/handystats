57f1bfc1e29da5fa69128c2706a5746341025bce
Danil Osherov <shindo@yandex-team.ru>
2014-03-07 21:58:44 +0400

    message queue: message queue implementation has been replaced with waitfree queue (taken from Boost's Atomic usage examples)

--- src/message_queue.cpp
+++ src/message_queue.cpp
@@ -1,6 +1,5 @@
 #include <memory>
-
-#include <tbb/concurrent_queue.h>
+#include <atomic>
 
 #include "events/event_message_impl.hpp"
 #include "system_stats_impl.hpp"
@@ -8,58 +7,114 @@
 
 namespace handystats { namespace message_queue {
 
-tbb::concurrent_queue<std::shared_ptr<events::event_message>>* event_message_queue = nullptr;
+
+/*
+ * Taken from Boost's Atomic usage examples.
+ */
+template <typename T>
+struct waitfree_queue
+{
+	struct node
+	{
+		node* next;
+		T data;
+	};
+
+	void push(const T& data)
+	{
+		node* n = new node;
+		n->data = data;
+		node* stale_head = head_.load(std::memory_order_relaxed);
+		do {
+			n->next = stale_head;
+		}
+		while (!head_.compare_exchange_weak(stale_head, n, std::memory_order_release));
+	}
+
+	node* pop_all(void)
+	{
+		node* last = pop_all_reverse();
+		node* first = 0;
+		while(last) {
+			node* tmp = last;
+			last = last->next;
+			tmp->next = first;
+			first = tmp;
+		}
+		return first;
+	}
+
+	// alternative interface if ordering is of no importance
+	node* pop_all_reverse(void)
+	{
+		// TODO: memory order should be std::memory_order_consume here, but it causes compilation error
+		return head_.exchange(0, std::memory_order_acquire);
+	}
+
+	waitfree_queue() : head_(0) {}
+
+	bool empty() const {
+		return !head_.load();
+	}
+
+	std::atomic<node*> head_;
+};
+
+waitfree_queue<std::shared_ptr<events::event_message>>* event_message_queue = nullptr;
+waitfree_queue<std::shared_ptr<events::event_message>>::node* popped_head = nullptr;
+
+std::atomic<size_t> size_(0);
 
 void push_event_message(std::shared_ptr<events::event_message> message) {
 	if (event_message_queue) {
 		event_message_queue->push(message);
+		++size_;
 	}
 }
 
 std::shared_ptr<events::event_message> pop_event_message() {
 	std::shared_ptr<events::event_message> message;
 
-	if (event_message_queue) {
-		auto pop_start_time = chrono::default_clock::now();
-		event_message_queue->try_pop(message);
-		auto pop_end_time = chrono::default_clock::now();
+	if (!popped_head && event_message_queue) {
+		popped_head = event_message_queue->pop_all();
+	}
 
-		if (message) {
-			message_pop_time.set(std::chrono::duration_cast<chrono::default_duration>(pop_end_time - pop_start_time).count(), pop_end_time);
-			message_queue_size.set(event_message_queue->unsafe_size(), pop_end_time);
-		}
+	if (popped_head) {
+		auto tmp = popped_head;
+		message = popped_head->data;
+		--size_;
+
+		popped_head = popped_head->next;
+		delete tmp;
 	}
 
 	return message;
 }
 
 bool empty() {
-	if (!event_message_queue) {
-		return true;
-	}
-
-	return event_message_queue->empty();
+//	return (!event_message_queue || event_message_queue->empty()) && !popped_head;
+	return size_.load() == 0;
 }
 
 size_t size() {
-	if (!event_message_queue) {
-		return 0;
-	}
-	return event_message_queue->size();
+	return size_.load();
 }
 
 void initialize() {
 	if (!event_message_queue) {
-		event_message_queue = new tbb::concurrent_queue<std::shared_ptr<events::event_message>>();
+		event_message_queue = new waitfree_queue<std::shared_ptr<events::event_message>>();
+		size_.store(0);
 	}
 }
 
 void finalize() {
-	if (event_message_queue) {
-		event_message_queue->clear();
-		delete event_message_queue;
-		event_message_queue = nullptr;
+	while (!empty()) {
+		pop_event_message();
 	}
+	size_.store(0);
+
+	delete event_message_queue;
+	event_message_queue = nullptr;
 }
 
 }} // namespace handystats::message_queue

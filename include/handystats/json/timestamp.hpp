ed559843a866705a3db3a38e7a343788d05565ae
Danil Osherov <shindo@yandex-team.ru>
2014-06-10 17:39:23 +0400

    json: support for writing to buffer and string has been added to timestamp json writer.

--- include/handystats/json/timestamp.hpp
+++ include/handystats/json/timestamp.hpp
@@ -3,6 +3,8 @@
 
 #include <handystats/chrono.hpp>
 #include <handystats/rapidjson/document.h>
+#include <handystats/rapidjson/stringbuffer.h>
+#include <handystats/rapidjson/prettywriter.h>
 
 namespace handystats { namespace json {
 
@@ -19,6 +21,26 @@ inline void write_to_json_value(const chrono::default_time_point& timestamp, rap
 	json_value->SetUint64(std::chrono::duration_cast<time_duration>(system_timestamp.time_since_epoch()).count());
 }
 
+template<typename StringBuffer, typename Allocator>
+inline void write_to_json_buffer(const chrono::default_time_point& timestamp, StringBuffer* buffer, Allocator& allocator) {
+	rapidjson::Value json_value;
+	write_to_json_value(timestamp, &json_value, allocator);
+
+	if (!buffer) {
+		buffer = new StringBuffer();
+	}
+
+	rapidjson::PrettyWriter<StringBuffer> writer(*buffer);
+	json_value.Accept(writer);
+}
+
+template<typename Allocator>
+inline std::string write_to_json_string(const chrono::default_time_point& timestamp, Allocator&& allocator = Allocator()) {
+	rapidjson::GenericStringBuffer<rapidjson::UTF8<>, Allocator> buffer(&allocator);
+	write_to_json_buffer(timestamp, &buffer, allocator);
+
+	return std::string(buffer.GetString(), buffer.GetSize());
+}
 }} // namespace handystats::json
 
 #endif // HANDYSTATS_JSON_TIMESTAMP_H_

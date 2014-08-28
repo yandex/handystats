b557c6cfd967fe05dcc9c6140487fea978ff7da2
Danil Osherov <shindo@yandex-team.ru>
2014-08-28 14:56:43 +0400

    tests: C bindings test added.

---
+++ tests/c-binding/check.cpp
@@ -0,0 +1,165 @@
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include <handystats/metrics_dump.hpp>
+
+#include "message_queue_impl.hpp"
+#include "test.h"
+
+TEST(CBindingTest, TestGauge) {
+	auto metrics_dump = HANDY_METRICS_DUMP();
+
+	ASSERT_TRUE(metrics_dump->find(TEST_GAUGE_NAME) != metrics_dump->end());
+
+	const auto& test_gauge = boost::get<handystats::metrics::gauge>(metrics_dump->at(TEST_GAUGE_NAME));
+
+	ASSERT_NEAR(test_gauge.values.min(), TEST_GAUGE_MIN, 1E-9);
+
+	ASSERT_NEAR(test_gauge.values.max(), TEST_GAUGE_MAX, 1E-9);
+
+	ASSERT_EQ(test_gauge.values.count(), TEST_GAUGE_MAX - TEST_GAUGE_MIN + 1);
+
+	ASSERT_NEAR(test_gauge.values.mean(), (TEST_GAUGE_MIN + TEST_GAUGE_MAX) / 2.0, 1E-9);
+}
+
+TEST(CBindingTest, TestCounter) {
+	auto metrics_dump = HANDY_METRICS_DUMP();
+
+	ASSERT_TRUE(metrics_dump->find(TEST_COUNTER_NAME) != metrics_dump->end());
+
+	const auto& test_counter = boost::get<handystats::metrics::counter>(metrics_dump->at(TEST_COUNTER_NAME));
+
+	ASSERT_EQ(test_counter.incr_deltas.count(), TEST_COUNTER_INCR_COUNT);
+	ASSERT_NEAR(test_counter.incr_deltas.min(), TEST_COUNTER_INCR_DELTA, 1E-9);
+	ASSERT_NEAR(test_counter.incr_deltas.max(), TEST_COUNTER_INCR_DELTA, 1E-9);
+
+	ASSERT_EQ(test_counter.decr_deltas.count(), TEST_COUNTER_DECR_COUNT);
+	ASSERT_NEAR(test_counter.decr_deltas.min(), TEST_COUNTER_DECR_DELTA, 1E-9);
+	ASSERT_NEAR(test_counter.decr_deltas.max(), TEST_COUNTER_DECR_DELTA, 1E-9);
+
+	ASSERT_EQ(test_counter.values.count(), TEST_COUNTER_INCR_COUNT + TEST_COUNTER_DECR_COUNT + 1);
+}
+
+TEST(CBindingTest, TestScopedCounter) {
+	auto metrics_dump = HANDY_METRICS_DUMP();
+
+	ASSERT_TRUE(metrics_dump->find(TEST_SCOPED_COUNTER_NAME) != metrics_dump->end());
+
+	const auto& test_scoped_counter = boost::get<handystats::metrics::counter>(metrics_dump->at(TEST_SCOPED_COUNTER_NAME));
+
+	ASSERT_EQ(test_scoped_counter.values.count(), TEST_SCOPED_COUNTER_COUNT * 2 + 1);
+	ASSERT_NEAR(test_scoped_counter.value, 0, 1E-9);
+
+	ASSERT_EQ(test_scoped_counter.incr_deltas.count(), TEST_SCOPED_COUNTER_COUNT);
+	ASSERT_NEAR(test_scoped_counter.incr_deltas.min(), TEST_SCOPED_COUNTER_DELTA, 1E-9);
+	ASSERT_NEAR(test_scoped_counter.incr_deltas.max(), TEST_SCOPED_COUNTER_DELTA, 1E-9);
+
+	ASSERT_EQ(test_scoped_counter.decr_deltas.count(), TEST_SCOPED_COUNTER_COUNT);
+	ASSERT_NEAR(test_scoped_counter.decr_deltas.min(), TEST_SCOPED_COUNTER_DELTA, 1E-9);
+	ASSERT_NEAR(test_scoped_counter.decr_deltas.max(), TEST_SCOPED_COUNTER_DELTA, 1E-9);
+}
+
+TEST(CBindingTest, TestTimer) {
+	auto metrics_dump = HANDY_METRICS_DUMP();
+
+	ASSERT_TRUE(metrics_dump->find(TEST_TIMER_NAME) != metrics_dump->end());
+
+	const auto& test_timer = boost::get<handystats::metrics::timer>(metrics_dump->at(TEST_TIMER_NAME));
+
+	ASSERT_EQ(test_timer.values.count(), TEST_TIMER_NANOSLEEP_COUNT);
+	ASSERT_GE(test_timer.values.min(), TEST_TIMER_NANOSLEEP_COUNT / 1000.0);
+}
+
+TEST(CBindingTest, TestScopedTimer) {
+	auto metrics_dump = HANDY_METRICS_DUMP();
+
+	ASSERT_TRUE(metrics_dump->find(TEST_SCOPED_TIMER_NAME) != metrics_dump->end());
+
+	const auto& test_scoped_timer = boost::get<handystats::metrics::timer>(metrics_dump->at(TEST_SCOPED_TIMER_NAME));
+
+	ASSERT_EQ(test_scoped_timer.values.count(), TEST_SCOPED_TIMER_NANOSLEEP_COUNT);
+	ASSERT_GE(test_scoped_timer.values.min(), TEST_SCOPED_TIMER_NANOSLEEP_COUNT / 1000.0);
+}
+
+TEST(CBindingTest, TestBoolAttr) {
+	auto metrics_dump = HANDY_METRICS_DUMP();
+
+	ASSERT_TRUE(metrics_dump->find(TEST_BOOL_ATTR_NAME) != metrics_dump->end());
+
+	const auto& test_bool_attr = boost::get<handystats::metrics::attribute>(metrics_dump->at(TEST_BOOL_ATTR_NAME));
+
+	ASSERT_EQ(boost::get<bool>(test_bool_attr.value), TEST_BOOL_ATTR_VALUE);
+}
+
+TEST(CBindingTest, TestDoubleAttr) {
+	auto metrics_dump = HANDY_METRICS_DUMP();
+
+	ASSERT_TRUE(metrics_dump->find(TEST_DOUBLE_ATTR_NAME) != metrics_dump->end());
+
+	const auto& test_double_attr = boost::get<handystats::metrics::attribute>(metrics_dump->at(TEST_DOUBLE_ATTR_NAME));
+
+	ASSERT_NEAR(boost::get<double>(test_double_attr.value), TEST_DOUBLE_ATTR_VALUE, 1E-9);
+}
+
+TEST(CBindingTest, TestIntAttr) {
+	auto metrics_dump = HANDY_METRICS_DUMP();
+
+	ASSERT_TRUE(metrics_dump->find(TEST_INT_ATTR_NAME) != metrics_dump->end());
+
+	const auto& test_int_attr = boost::get<handystats::metrics::attribute>(metrics_dump->at(TEST_INT_ATTR_NAME));
+
+	ASSERT_EQ(boost::get<int>(test_int_attr.value), TEST_INT_ATTR_VALUE);
+}
+
+TEST(CBindingTest, TestUintAttr) {
+	auto metrics_dump = HANDY_METRICS_DUMP();
+
+	ASSERT_TRUE(metrics_dump->find(TEST_UINT_ATTR_NAME) != metrics_dump->end());
+
+	const auto& test_uint_attr = boost::get<handystats::metrics::attribute>(metrics_dump->at(TEST_UINT_ATTR_NAME));
+
+	ASSERT_EQ(boost::get<unsigned>(test_uint_attr.value), TEST_UINT_ATTR_VALUE);
+}
+
+TEST(CBindingTest, TestInt64Attr) {
+	auto metrics_dump = HANDY_METRICS_DUMP();
+
+	ASSERT_TRUE(metrics_dump->find(TEST_INT64_ATTR_NAME) != metrics_dump->end());
+
+	const auto& test_int64_attr = boost::get<handystats::metrics::attribute>(metrics_dump->at(TEST_INT64_ATTR_NAME));
+
+	ASSERT_EQ(boost::get<int64_t>(test_int64_attr.value), TEST_INT64_ATTR_VALUE);
+}
+
+TEST(CBindingTest, TestUint64Attr) {
+	auto metrics_dump = HANDY_METRICS_DUMP();
+
+	ASSERT_TRUE(metrics_dump->find(TEST_UINT64_ATTR_NAME) != metrics_dump->end());
+
+	const auto& test_uint64_attr = boost::get<handystats::metrics::attribute>(metrics_dump->at(TEST_UINT64_ATTR_NAME));
+
+	ASSERT_EQ(boost::get<uint64_t>(test_uint64_attr.value), TEST_UINT64_ATTR_VALUE);
+}
+
+TEST(CBindingTest, TestStringAttr) {
+	auto metrics_dump = HANDY_METRICS_DUMP();
+
+	ASSERT_TRUE(metrics_dump->find(TEST_STRING_ATTR_NAME) != metrics_dump->end());
+
+	const auto& test_string_attr = boost::get<handystats::metrics::attribute>(metrics_dump->at(TEST_STRING_ATTR_NAME));
+
+	ASSERT_EQ(boost::get<std::string>(test_string_attr.value), TEST_STRING_ATTR_VALUE);
+}
+
+
+int check_tests(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+
+	while (!handystats::message_queue::empty()) {
+		std::this_thread::sleep_for(std::chrono::microseconds(1));
+	}
+	std::this_thread::sleep_for(std::chrono::milliseconds(20));
+
+	return RUN_ALL_TESTS();
+}
---
+++ tests/c-binding/test.c
@@ -0,0 +1,122 @@
+#include <time.h>
+
+#include <handystats/operation.h>
+#include <handystats/config.h>
+#include <handystats/measuring_points.h>
+
+#include "test.h"
+
+void run_test_gauge(void) {
+	int gauge_value;
+	for(gauge_value = TEST_GAUGE_MIN; gauge_value <= TEST_GAUGE_MAX; ++gauge_value) {
+		HANDY_GAUGE_SET(TEST_GAUGE_NAME, gauge_value);
+	}
+}
+
+void run_test_counter(void) {
+	HANDY_COUNTER_INIT(TEST_COUNTER_NAME, TEST_COUNTER_INIT_VALUE);
+
+	int incr_step;
+	for (incr_step = 0; incr_step < TEST_COUNTER_INCR_COUNT; ++incr_step) {
+		HANDY_COUNTER_INCREMENT(TEST_COUNTER_NAME, TEST_COUNTER_INCR_DELTA);
+	}
+
+	int decr_step;
+	for (decr_step = 0; decr_step < TEST_COUNTER_DECR_COUNT; ++decr_step) {
+		HANDY_COUNTER_DECREMENT(TEST_COUNTER_NAME, TEST_COUNTER_DECR_DELTA);
+	}
+}
+
+void run_test_scoped_counter(void) {
+	int cycle;
+	for (cycle = 0; cycle < TEST_SCOPED_COUNTER_COUNT; ++cycle) {
+		HANDY_COUNTER_SCOPE(TEST_SCOPED_COUNTER_NAME, TEST_SCOPED_COUNTER_DELTA);
+	}
+}
+
+void run_test_timer(void) {
+	struct timespec rqtp;
+	rqtp.tv_sec = 0;
+	rqtp.tv_nsec = TEST_TIMER_NANOSLEEP_TIME;
+
+	int cycle;
+
+	for (cycle = 0; cycle < TEST_TIMER_NANOSLEEP_COUNT; ++cycle) {
+		HANDY_TIMER_START(TEST_TIMER_NAME, cycle);
+	}
+
+	nanosleep(&rqtp, NULL);
+
+	for (cycle = 0; cycle < TEST_TIMER_NANOSLEEP_COUNT; ++cycle) {
+		HANDY_TIMER_STOP(TEST_TIMER_NAME, cycle);
+	}
+}
+
+void run_test_scoped_timer(void) {
+	struct timespec rqtp;
+	rqtp.tv_sec = 0;
+	rqtp.tv_nsec = TEST_SCOPED_TIMER_NANOSLEEP_TIME;
+
+	int cycle;
+
+	for (cycle = 0; cycle < TEST_SCOPED_TIMER_NANOSLEEP_COUNT; ++cycle) {
+		HANDY_TIMER_SCOPE(TEST_SCOPED_TIMER_NAME, cycle);
+		nanosleep(&rqtp, NULL);
+	}
+}
+
+void run_test_bool_attr(void) {
+	HANDY_ATTRIBUTE_SET_BOOL(TEST_BOOL_ATTR_NAME, TEST_BOOL_ATTR_VALUE);
+}
+
+void run_test_double_attr(void) {
+	HANDY_ATTRIBUTE_SET_DOUBLE(TEST_DOUBLE_ATTR_NAME, TEST_DOUBLE_ATTR_VALUE);
+}
+
+void run_test_int_attr(void) {
+	HANDY_ATTRIBUTE_SET_INT(TEST_INT_ATTR_NAME, TEST_INT_ATTR_VALUE);
+}
+
+void run_test_uint_attr(void) {
+	HANDY_ATTRIBUTE_SET_UINT(TEST_UINT_ATTR_NAME, TEST_UINT_ATTR_VALUE);
+}
+
+void run_test_int64_attr(void) {
+	HANDY_ATTRIBUTE_SET_INT64(TEST_INT64_ATTR_NAME, TEST_INT64_ATTR_VALUE);
+}
+
+void run_test_uint64_attr(void) {
+	HANDY_ATTRIBUTE_SET_UINT64(TEST_UINT64_ATTR_NAME, TEST_UINT64_ATTR_VALUE);
+}
+
+void run_test_string_attr(void) {
+	HANDY_ATTRIBUTE_SET_STRING(TEST_STRING_ATTR_NAME, TEST_STRING_ATTR_VALUE);
+}
+
+void run_test_attr(void) {
+	run_test_bool_attr();
+	run_test_double_attr();
+	run_test_int_attr();
+	run_test_uint_attr();
+	run_test_int64_attr();
+	run_test_uint64_attr();
+	run_test_string_attr();
+}
+
+int main(int argc, char** argv) {
+	HANDY_CONFIG_JSON("{\"metrics-dump\":{\"interval\": 20, \"to-json\": true}}");
+	HANDY_INIT();
+
+	run_test_gauge();
+	run_test_counter();
+	run_test_scoped_counter();
+	run_test_timer();
+	run_test_scoped_timer();
+	run_test_attr();
+
+	int ret = check_tests(argc, argv);
+
+	HANDY_FINALIZE();
+
+	return ret;
+}

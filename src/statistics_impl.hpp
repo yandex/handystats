f3ec3d89dc060f19f214ef951a573234583570ef
Danil Osherov <shindo@yandex-team.ru>
2014-10-14 19:42:59 +0400

    statistics: truncating mechanics for statistics data added.

--- src/statistics_impl.hpp
+++ src/statistics_impl.hpp
@@ -20,6 +20,11 @@ struct statistics::data {
 			const time_point& timestamp
 		)
 		const;
+	double truncate_interval_data(
+			const double& data, const time_point& data_timestamp,
+			const time_point& timestamp
+		)
+		const;
 	double update_interval_data(
 			const double& data, const time_point& data_timestamp,
 			const value_type& value, const time_point& timestamp
@@ -28,12 +33,14 @@ struct statistics::data {
 
 	// histogram (in-place)
 	void shift_histogram(const time_point& timestamp);
+	void truncate_histogram(const time_point& timestamp);
 	void update_histogram(const value_type& value, const time_point& timestamp);
 
 	void reset();
 
 	void update(const value_type& value, const time_point& timestamp = clock::now());
 	void update_time(const time_point& timestamp = clock::now());
+	void truncate_time(const time_point& timestamp);
 
 	value_type m_value;
 	value_type m_min;

58e4ad27b9080b039ace49d34746eab8f2a32d85
Danil Osherov <shindo@yandex-team.ru>
2014-10-21 16:51:03 +0400

    serialization: json serialization interface added (template loads and dumps functions). serialization: json serialization specifications for statistics::data added.

---
+++ src/serialization/json.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2014 Yandex LLC. All rights reserved.
+
+#include <stdexcept>
+
+#include <handystats/utils/rapidjson_writer.hpp>
+
+#include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+
+#include "serialization/json_impl.hpp"
+#include "serialization/rapidjson_impl.hpp"
+
+using namespace rapidjson;
+
+namespace handystats { namespace serialization { namespace json {
+
+template <>
+statistics::data loads<statistics::data>(const std::string& dump) {
+	Document dump_value;
+	dump_value.Parse<0>(dump.c_str());
+	if (dump_value.HasParseError()) {
+		throw std::invalid_argument(dump_value.GetParseError());
+	}
+
+	statistics::data data;
+	rapidjson::load_value(data, dump_value);
+
+	return data;
+}
+
+template <>
+std::string dumps<statistics::data>(const statistics::data& data) {
+	Value json_dump;
+	MemoryPoolAllocator<> allocator;
+
+	rapidjson::fill_value(json_dump, data, allocator);
+
+	return utils::rapidjson::to_string <
+				Value, StringBuffer,
+				Writer<StringBuffer>
+			>
+			(json_dump);
+}
+
+}}} // namespace handystats::serialization::json
---
+++ src/serialization/json_impl.hpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2014 Yandex LLC. All rights reserved.
+
+#ifndef HANDYSTATS_SERIALIZATION_JSON_IMPL_HPP_
+#define HANDYSTATS_SERIALIZATION_JSON_IMPL_HPP_
+
+#include <string>
+
+#include "statistics_impl.hpp"
+
+namespace handystats { namespace serialization { namespace json {
+
+template <typename Object>
+Object loads(const std::string&);
+
+template <typename Object>
+std::string dumps(const Object&);
+
+// specializations for statistics::data
+template <>
+statistics::data loads<statistics::data>(const std::string& dump);
+template <>
+std::string dumps<statistics::data>(const statistics::data& data);
+
+}}} // namespace handystats::serialization::json
+
+#endif // HANDYSTATS_SERIALIZATION_JSON_IMPL_HPP_

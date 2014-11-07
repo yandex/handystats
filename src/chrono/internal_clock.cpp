bddd9d1665277fae244c4f263b5c73443cfc1a12
Danil Osherov <shindo@yandex-team.ru>
2014-11-07 21:54:05 +0300

    chrono: CLOCK_MONOTONIC_RAW clock source for internal clock added.

--- src/chrono/internal_clock.cpp
+++ src/chrono/internal_clock.cpp
@@ -47,6 +47,7 @@ namespace {
 enum clock_source_t {
 	CS_RDTSC,
 	CS_RDTSCP,
+	CS_CLOCK_MONOTONIC_RAW,
 	CS_CLOCK_MONOTONIC,
 	CS_CLOCK_REALTIME
 };
@@ -62,7 +63,14 @@ clock_source_t get_available_clock_source() {
 		}
 	}
 	else {
-		// fallback to POSIX clocks
+#ifdef CLOCK_MONOTONIC_RAW
+		{
+			timespec test_tp;
+			if (clock_gettime(CLOCK_MONOTONIC_RAW, &test_tp) == 0) {
+				return CS_CLOCK_MONOTONIC_RAW;
+			}
+		}
+#endif // CLOCK_MONOTONIC_RAW
 		if (sysconf(_SC_MONOTONIC_CLOCK) >= 0) {
 			return CS_CLOCK_MONOTONIC;
 		}
@@ -81,16 +89,24 @@ void init_clock_source() {
 
 
 uint64_t get_nanoseconds() {
-	timespec tm;
+	int posix_clock = -1;
 
-	int posix_clock;
-	if (sysconf(_SC_MONOTONIC_CLOCK) >= 0) {
+#ifdef CLOCK_MONOTONIC_RAW
+	timespec test_tp;
+	if (clock_gettime(CLOCK_MONOTONIC_RAW, &test_tp) == 0) {
+		posix_clock = CLOCK_MONOTONIC_RAW;
+	}
+#endif
+
+	if (posix_clock == -1 && sysconf(_SC_MONOTONIC_CLOCK) >= 0) {
 		posix_clock = CLOCK_MONOTONIC;
 	}
-	else {
+
+	if (posix_clock == -1) {
 		posix_clock = CLOCK_REALTIME;
 	}
 
+	timespec tm;
 	clock_gettime(posix_clock, &tm);
 
 	return (uint64_t)tm.tv_sec * (uint64_t)1E9 + tm.tv_nsec;
@@ -186,6 +202,14 @@ time_point internal_clock::now() {
 	case CS_RDTSCP:
 		{
 			return time_point(duration(rdtscp(), time_unit::CYCLE), clock_type::INTERNAL_CLOCK);
+		}
+	case CS_CLOCK_MONOTONIC_RAW:
+		{
+			timespec tm;
+#ifdef CLOCK_MONOTONIC_RAW
+			clock_gettime(CLOCK_MONOTONIC_RAW, &tm);
+#endif
+			return time_point(duration((int64_t)tm.tv_sec * (int64_t)1E9 + tm.tv_nsec, time_unit::NSEC), clock_type::INTERNAL_CLOCK);
 		}
 	case CS_CLOCK_MONOTONIC:
 		{

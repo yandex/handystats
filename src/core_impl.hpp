65dcfbb8cc91921cc76a53ca9540e1e34c955ee3
Danil Osherov <shindo@yandex-team.ru>
2014-11-12 20:40:55 +0300

    LGPL3 licensing.

--- src/core_impl.hpp
+++ src/core_impl.hpp
@@ -1,4 +1,21 @@
-// Copyright (c) 2014 Yandex LLC. All rights reserved.
+/*
+  Copyright (c) 2014 Yandex LLC. All rights reserved.
+
+  This file is part of Handystats.
+
+  Handystats is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  Handystats is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef HANDYSTATS_CORE_IMPL_HPP_
 #define HANDYSTATS_CORE_IMPL_HPP_

1900f8d35cd8d8b6fa2b0c931ee9cec8d97c3825
Danil Osherov <shindo@yandex-team.ru>
2014-12-02 20:10:16 +0300

    tests/c-binding: use new printf-like syntax for metric names.

--- tests/c-binding/check.cpp
+++ tests/c-binding/check.cpp
@@ -34,9 +34,9 @@
 TEST(CBindingTest, TestGauge) {
 	auto metrics_dump = HANDY_METRICS_DUMP();
 
-	ASSERT_TRUE(metrics_dump->first.find(TEST_GAUGE_NAME) != metrics_dump->first.end());
+	ASSERT_TRUE(metrics_dump->first.find("test." TEST_GAUGE_NAME) != metrics_dump->first.end());
 
-	const auto& test_gauge_values = metrics_dump->first.at(TEST_GAUGE_NAME);
+	const auto& test_gauge_values = metrics_dump->first.at("test." TEST_GAUGE_NAME);
 
 	ASSERT_NEAR(test_gauge_values.get<handystats::statistics::tag::min>(), TEST_GAUGE_MIN, 1E-9);
 
@@ -50,9 +50,9 @@ TEST(CBindingTest, TestGauge) {
 TEST(CBindingTest, TestCounter) {
 	auto metrics_dump = HANDY_METRICS_DUMP();
 
-	ASSERT_TRUE(metrics_dump->first.find(TEST_COUNTER_NAME) != metrics_dump->first.end());
+	ASSERT_TRUE(metrics_dump->first.find("test." TEST_COUNTER_NAME) != metrics_dump->first.end());
 
-	const auto& test_counter_values = metrics_dump->first.at(TEST_COUNTER_NAME);
+	const auto& test_counter_values = metrics_dump->first.at("test." TEST_COUNTER_NAME);
 
 	ASSERT_EQ(test_counter_values.get<handystats::statistics::tag::count>(), TEST_COUNTER_INCR_COUNT + TEST_COUNTER_DECR_COUNT + 1);
 }
@@ -60,9 +60,9 @@ TEST(CBindingTest, TestCounter) {
 TEST(CBindingTest, TestScopedCounter) {
 	auto metrics_dump = HANDY_METRICS_DUMP();
 
-	ASSERT_TRUE(metrics_dump->first.find(TEST_SCOPED_COUNTER_NAME) != metrics_dump->first.end());
+	ASSERT_TRUE(metrics_dump->first.find("test." TEST_SCOPED_COUNTER_NAME) != metrics_dump->first.end());
 
-	const auto& test_scoped_counter = metrics_dump->first.at(TEST_SCOPED_COUNTER_NAME);
+	const auto& test_scoped_counter = metrics_dump->first.at("test." TEST_SCOPED_COUNTER_NAME);
 
 	ASSERT_EQ(test_scoped_counter.get<handystats::statistics::tag::count>(), TEST_SCOPED_COUNTER_COUNT * 2 + 1);
 	ASSERT_NEAR(test_scoped_counter.get<handystats::statistics::tag::value>(), 0, 1E-9);
@@ -71,9 +71,9 @@ TEST(CBindingTest, TestScopedCounter) {
 TEST(CBindingTest, TestTimer) {
 	auto metrics_dump = HANDY_METRICS_DUMP();
 
-	ASSERT_TRUE(metrics_dump->first.find(TEST_TIMER_NAME) != metrics_dump->first.end());
+	ASSERT_TRUE(metrics_dump->first.find("test." TEST_TIMER_NAME) != metrics_dump->first.end());
 
-	const auto& test_timer_values = metrics_dump->first.at(TEST_TIMER_NAME);
+	const auto& test_timer_values = metrics_dump->first.at("test." TEST_TIMER_NAME);
 
 	ASSERT_EQ(test_timer_values.get<handystats::statistics::tag::count>(), TEST_TIMER_NANOSLEEP_COUNT);
 	ASSERT_GE(test_timer_values.get<handystats::statistics::tag::min>(), TEST_TIMER_NANOSLEEP_COUNT / 1000.0);
@@ -82,9 +82,9 @@ TEST(CBindingTest, TestTimer) {
 TEST(CBindingTest, TestScopedTimer) {
 	auto metrics_dump = HANDY_METRICS_DUMP();
 
-	ASSERT_TRUE(metrics_dump->first.find(TEST_SCOPED_TIMER_NAME) != metrics_dump->first.end());
+	ASSERT_TRUE(metrics_dump->first.find("test." TEST_SCOPED_TIMER_NAME) != metrics_dump->first.end());
 
-	const auto& test_scoped_timer = metrics_dump->first.at(TEST_SCOPED_TIMER_NAME);
+	const auto& test_scoped_timer = metrics_dump->first.at("test." TEST_SCOPED_TIMER_NAME);
 
 	ASSERT_EQ(test_scoped_timer.get<handystats::statistics::tag::count>(), TEST_SCOPED_TIMER_NANOSLEEP_COUNT);
 	ASSERT_GE(test_scoped_timer.get<handystats::statistics::tag::min>(), TEST_SCOPED_TIMER_NANOSLEEP_COUNT / 1000.0);
@@ -93,9 +93,9 @@ TEST(CBindingTest, TestScopedTimer) {
 TEST(CBindingTest, TestBoolAttr) {
 	auto metrics_dump = HANDY_METRICS_DUMP();
 
-	ASSERT_TRUE(metrics_dump->second.find(TEST_BOOL_ATTR_NAME) != metrics_dump->second.end());
+	ASSERT_TRUE(metrics_dump->second.find("test." TEST_BOOL_ATTR_NAME) != metrics_dump->second.end());
 
-	const auto& test_bool_attr = metrics_dump->second.at(TEST_BOOL_ATTR_NAME);
+	const auto& test_bool_attr = metrics_dump->second.at("test." TEST_BOOL_ATTR_NAME);
 
 	ASSERT_EQ(boost::get<bool>(test_bool_attr.value()), TEST_BOOL_ATTR_VALUE);
 }
@@ -103,9 +103,9 @@ TEST(CBindingTest, TestBoolAttr) {
 TEST(CBindingTest, TestDoubleAttr) {
 	auto metrics_dump = HANDY_METRICS_DUMP();
 
-	ASSERT_TRUE(metrics_dump->second.find(TEST_DOUBLE_ATTR_NAME) != metrics_dump->second.end());
+	ASSERT_TRUE(metrics_dump->second.find("test." TEST_DOUBLE_ATTR_NAME) != metrics_dump->second.end());
 
-	const auto& test_double_attr = metrics_dump->second.at(TEST_DOUBLE_ATTR_NAME);
+	const auto& test_double_attr = metrics_dump->second.at("test." TEST_DOUBLE_ATTR_NAME);
 
 	ASSERT_NEAR(boost::get<double>(test_double_attr.value()), TEST_DOUBLE_ATTR_VALUE, 1E-9);
 }
@@ -113,9 +113,9 @@ TEST(CBindingTest, TestDoubleAttr) {
 TEST(CBindingTest, TestIntAttr) {
 	auto metrics_dump = HANDY_METRICS_DUMP();
 
-	ASSERT_TRUE(metrics_dump->second.find(TEST_INT_ATTR_NAME) != metrics_dump->second.end());
+	ASSERT_TRUE(metrics_dump->second.find("test." TEST_INT_ATTR_NAME) != metrics_dump->second.end());
 
-	const auto& test_int_attr = metrics_dump->second.at(TEST_INT_ATTR_NAME);
+	const auto& test_int_attr = metrics_dump->second.at("test." TEST_INT_ATTR_NAME);
 
 	ASSERT_EQ(boost::get<int>(test_int_attr.value()), TEST_INT_ATTR_VALUE);
 }
@@ -123,9 +123,9 @@ TEST(CBindingTest, TestIntAttr) {
 TEST(CBindingTest, TestUintAttr) {
 	auto metrics_dump = HANDY_METRICS_DUMP();
 
-	ASSERT_TRUE(metrics_dump->second.find(TEST_UINT_ATTR_NAME) != metrics_dump->second.end());
+	ASSERT_TRUE(metrics_dump->second.find("test." TEST_UINT_ATTR_NAME) != metrics_dump->second.end());
 
-	const auto& test_uint_attr = metrics_dump->second.at(TEST_UINT_ATTR_NAME);
+	const auto& test_uint_attr = metrics_dump->second.at("test." TEST_UINT_ATTR_NAME);
 
 	ASSERT_EQ(boost::get<unsigned>(test_uint_attr.value()), TEST_UINT_ATTR_VALUE);
 }
@@ -133,9 +133,9 @@ TEST(CBindingTest, TestUintAttr) {
 TEST(CBindingTest, TestInt64Attr) {
 	auto metrics_dump = HANDY_METRICS_DUMP();
 
-	ASSERT_TRUE(metrics_dump->second.find(TEST_INT64_ATTR_NAME) != metrics_dump->second.end());
+	ASSERT_TRUE(metrics_dump->second.find("test." TEST_INT64_ATTR_NAME) != metrics_dump->second.end());
 
-	const auto& test_int64_attr = metrics_dump->second.at(TEST_INT64_ATTR_NAME);
+	const auto& test_int64_attr = metrics_dump->second.at("test." TEST_INT64_ATTR_NAME);
 
 	ASSERT_EQ(boost::get<int64_t>(test_int64_attr.value()), TEST_INT64_ATTR_VALUE);
 }
@@ -143,9 +143,9 @@ TEST(CBindingTest, TestInt64Attr) {
 TEST(CBindingTest, TestUint64Attr) {
 	auto metrics_dump = HANDY_METRICS_DUMP();
 
-	ASSERT_TRUE(metrics_dump->second.find(TEST_UINT64_ATTR_NAME) != metrics_dump->second.end());
+	ASSERT_TRUE(metrics_dump->second.find("test." TEST_UINT64_ATTR_NAME) != metrics_dump->second.end());
 
-	const auto& test_uint64_attr = metrics_dump->second.at(TEST_UINT64_ATTR_NAME);
+	const auto& test_uint64_attr = metrics_dump->second.at("test." TEST_UINT64_ATTR_NAME);
 
 	ASSERT_EQ(boost::get<uint64_t>(test_uint64_attr.value()), TEST_UINT64_ATTR_VALUE);
 }
@@ -153,9 +153,9 @@ TEST(CBindingTest, TestUint64Attr) {
 TEST(CBindingTest, TestStringAttr) {
 	auto metrics_dump = HANDY_METRICS_DUMP();
 
-	ASSERT_TRUE(metrics_dump->second.find(TEST_STRING_ATTR_NAME) != metrics_dump->second.end());
+	ASSERT_TRUE(metrics_dump->second.find("test." TEST_STRING_ATTR_NAME) != metrics_dump->second.end());
 
-	const auto& test_string_attr = metrics_dump->second.at(TEST_STRING_ATTR_NAME);
+	const auto& test_string_attr = metrics_dump->second.at("test." TEST_STRING_ATTR_NAME);
 
 	ASSERT_EQ(boost::get<std::string>(test_string_attr.value()), TEST_STRING_ATTR_VALUE);
 }
